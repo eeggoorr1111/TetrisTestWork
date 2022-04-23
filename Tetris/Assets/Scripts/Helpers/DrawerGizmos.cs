acfd01a4807de442f117cd6bd64382354c33d8ec
eeggoorr1111 <egormywork@gmail.com>
2022-04-23 12:41:18 +0500

    Создание тулзы для отрисовки гизм

---
+++ Tetris/Assets/Scripts/Helpers/DrawerGizmos.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace Tetris
+{
+    public sealed class DrawerGizmos : MonoBehaviour
+    {
+        public static DrawerGizmos Instance => _instance;
+
+
+        private event Action _onDrawGizmos;
+        private static DrawerGizmos _instance;
+
+
+        public void SubscribeOnDrawGizmos(Action callbackArg)
+        {
+            _onDrawGizmos -= callbackArg;
+            _onDrawGizmos += callbackArg;
+        }
+        public void UnsubscribeOnDrawGizmos(Action callbackArg)
+        {
+            _onDrawGizmos -= callbackArg;
+        }
+
+
+        private void OnEnable()
+        {
+            if (_instance != null)
+                Debug.LogError($"На сцене присутствует более одного { GetType().Name }", this);
+            _instance = this;
+        }
+        private void OnDisable()
+        {
+            _instance = null;
+            _onDrawGizmos = null;
+        }
+        private void OnDrawGizmos()
+        {
+            if (_onDrawGizmos != null)
+                _onDrawGizmos.Invoke();
+        }
+    }
+}
+
